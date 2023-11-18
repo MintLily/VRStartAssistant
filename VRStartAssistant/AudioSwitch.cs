ed86c42d61c6da399fe76e0272c17a4930f3f19a
Lily <admin@mintlily.lgbt>
2023-11-18 17:37:53 -0500

    Changed audio switching lib

--- VRStartAssistant/AudioSwitch.cs
+++ VRStartAssistant/AudioSwitch.cs
@@ -1,69 +1,28 @@
-﻿// Came from https://github.com/RequiDev/AudioDeviceWatcher
-using NAudio.CoreAudioApi;
-using NAudio.CoreAudioApi.Interfaces;
-using Serilog;
-using VRStartAssistant.COM;
+﻿using Serilog;
+using AudioSwitcher.AudioApi.CoreAudio;
 
 namespace VRStartAssistant;
 
-internal class AudioSwitch : IMMNotificationClient {
-    // private string _presetInputDevice = null!;
-    private readonly string _presetOutputDevice = "{0.0.0.00000000}.{7bcc81d8-2edd-4045-9234-2d8bbc8b4847}";
+public class AudioSwitch {
+    public AudioSwitch() => Log.Information("[{0}] Setting up {Name} :: {Description}", "MODULE", "AudioSwitcher", "Automatically switches to specific audio devices");
+    
+    private readonly string _indexUsbAudioAdapterGuid = "1f6321e0-59f8-4a24-933c-6c3918f6262a";
+    private readonly string _soudcoreVrP10Guid = "feef26a6-db77-42e9-837d-4152d82fdac6";
 
-    private readonly MMDeviceEnumerator _deviceEnumerator;
-    private readonly PolicyConfigClient _policyConfigClient;
-
-    public AudioSwitch() {
-        Log.Information("[{0}] Setting up {Name} :: {Description}", "MODULE", "AudioSwitcher", "Automatically switches to VR USB Audio");
-        _deviceEnumerator = new MMDeviceEnumerator();
-        _policyConfigClient = new PolicyConfigClient();
-        _deviceEnumerator.RegisterEndpointNotificationCallback(this);
-    }
-
-    public void OnDefaultDeviceChanged(DataFlow flow, Role role, string defaultDeviceId) {
-        if (role != Role.Communications)
-            return;
-
-        switch (flow) {
-            /*case DataFlow.Capture when !string.IsNullOrEmpty(_presetInputDevice) &&
-                                       defaultDeviceId != _presetInputDevice &&
-                                       _deviceEnumerator.GetDevice(_presetInputDevice) != null:
-                SetDefaultAudioDevice(_presetInputDevice);
-                break;*/
-            case DataFlow.Render when !string.IsNullOrEmpty(_presetOutputDevice) &&
-                                      defaultDeviceId != _presetOutputDevice &&
-                                      _deviceEnumerator.GetDevice(_presetOutputDevice) != null:
-                SetDefaultAudioDevice(_presetOutputDevice);
-                Log.Information("Audio device changed to {Device}", _presetOutputDevice);
-                break;
+    public async Task Start() {
+        Log.Information("Attempting to set default audio device to {Device}...", _indexUsbAudioAdapterGuid);
+        try {
+            var controller = new CoreAudioController();
+            // var devices = await controller.GetPlaybackDevicesAsync(DeviceState.Active);
+            // foreach (var dev in devices) {
+            //     Console.WriteLine($"{dev.Name} - {dev.Id} - Is Muted: {dev.IsMuted}");
+            // }
+            var device = await controller.GetDeviceAsync(Guid.Parse(_indexUsbAudioAdapterGuid));
+            controller.DefaultPlaybackDevice = device;
+            Log.Information("Set audio device");
         }
-
-        Console.Beep();
-    }
-
-    private void SetDefaultAudioDevice(string deviceId) {
-        for (var i = 0; i <= (int)Role.Communications; i++) {
-            _policyConfigClient.SetDefaultEndpoint(deviceId, (Role)i);
+        catch (Exception e) {
+            Log.Error(e, "Failed to set default audio device");
         }
     }
-
-    #region not important
-
-    public void OnDeviceStateChanged(string deviceId, DeviceState newState) {
-        // not important
-    }
-
-    public void OnDeviceAdded(string pwstrDeviceId) {
-        // not important
-    }
-
-    public void OnDeviceRemoved(string deviceId) {
-        // not important
-    }
-
-    public void OnPropertyValueChanged(string pwstrDeviceId, PropertyKey key) {
-        // not important
-    }
-
-    #endregion
 }
