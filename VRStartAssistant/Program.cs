e459649ca96f69b2edf0de7375e674eb9a3f608f
Lily <admin@mintlily.lgbt>
2024-08-31 10:08:16 -0400

    version bump

--- VRStartAssistant/Program.cs
+++ VRStartAssistant/Program.cs
@@ -10,8 +10,8 @@ namespace VRStartAssistant;
 public static class Vars {
     public const string AppName = "VRStartAssistant";
     public const string WindowsTitle = "Automate VR Startup Things";
-    public const string AppVersion = "1.14.0";
-    public const int TargetConfigVersion = 10;
+    public const string AppVersion = "1.15.0";
+    public const int TargetConfigVersion = 11;
     internal static readonly string? BaseDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Documents", "Visual Studio Projects", "VROnStartAssistant", "Build");
 #if DEBUG
     public static bool IsDebug = true;
